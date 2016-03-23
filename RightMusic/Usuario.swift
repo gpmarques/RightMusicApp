688be432690c199baab45d2d2704999160941da7
Gabriel Bendia <gabrielbendia@gmail.com>
2016-03-23 14:17:38 -0300

    Classe usuario

---
+++ RightMusic/Usuario.swift
@@ -0,0 +1,15 @@
+//
+//  Usuario.swift
+//  RightMusic
+//
+//  Created by Gabriel Bendia on 3/23/16.
+//  Copyright © 2016 Guilherme Marques. All rights reserved.
+//
+
+import Foundation
+
+
+class Usuario {
+
+    var nome:String!
+}
